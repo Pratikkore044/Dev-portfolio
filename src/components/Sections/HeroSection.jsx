3df19e59d9a118fda63e8b7c871eb8d38d337ad1
Pratik <iampratikkore@gmail.com>
2026-02-23 14:24:55 +0530

    added profile picture

--- src/components/Sections/HeroSection.jsx
+++ src/components/Sections/HeroSection.jsx
@@ -11,7 +11,7 @@ import { FiGithub, FiLinkedin } from "react-icons/fi";
 import { useTheme } from "../../context/ThemeContext";
 
 
-import PROFILE_PIC from '../../assets/images/profile-img.jpg'
+import PROFILE_PIC from '../../assets/images/profile11.png'
 import { containerVariants, itemVariants } from "../../utils/helper";
 
 const HeroSection = () => {
